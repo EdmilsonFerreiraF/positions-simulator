5cfaf3968aa18d0bcbfa98922f388892bcceb797
root <root@DESKTOP-6BE3G1B.localdomain>
2022-06-24 20:19:44 -0300

    🚀 :rocket: [Add] LoadPositions

--- application/route/route.go
+++ application/route/route.go
@@ -1,5 +1,13 @@
 package route
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Route struct {
 	ID        string
 	ClientID  string
@@ -9,4 +17,40 @@ type Route struct {
 type Position struct {
 	Lat  float64
 	Long float64
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
+
+	f, err := os.Open("destinations/" + r.ID + ".txt")
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return nil
+		}
+
+		long, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return nil
+		}
+
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+
+	return nil
 }
