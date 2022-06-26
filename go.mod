bb6e6f7a444e163dc9aecd49959251cd9a7746a0
root <root@DESKTOP-6BE3G1B.localdomain>
2022-06-25 22:08:29 -0300

    🚀 :rocket: [Add] Main - init

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/EdmilsonFerreiraF/positions-simulator v0.0.0-20220624234154-0c4fd602c68a
 	github.com/confluentinc/confluent-kafka-go v1.9.0
+	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	route "github.com/EdmilsonFerreiraF/positions-simulator/application/route"
+	"github.com/codeedu/imersaofsfc2-simulator/application/route"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	route := route.Route{
